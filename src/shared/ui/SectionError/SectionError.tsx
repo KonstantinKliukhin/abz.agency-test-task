7b4ee5cb596845bfb0b210f4e9915c6895a3c565
Kliukhin Konstantin <kliukhinkonstantin@gmail.com>
2023-12-29 17:03:24 +0200

    #2 fix(section-error): fixed section error component reload functionality

--- src/shared/ui/SectionError/SectionError.tsx
+++ src/shared/ui/SectionError/SectionError.tsx
@@ -1,3 +1,5 @@
+'use client';
+import { useRouter } from 'next/navigation';
 import { FC } from 'react';
 
 import { Button } from '../Button/Button';
@@ -10,6 +12,8 @@ interface SectionErrorProps {
 }
 
 export const SectionError: FC<SectionErrorProps> = props => {
+  const router = useRouter();
+
   return (
     <Container size="m">
       <VStack justify="center" align="center" gapY={20}>
@@ -19,7 +23,7 @@ export const SectionError: FC<SectionErrorProps> = props => {
         <Typography align="center">
           {props.message}
         </Typography>
-        <Button>Reload</Button>
+        <Button onClick={() => router.refresh()}>Reload</Button>
       </VStack>
     </Container>
   );
